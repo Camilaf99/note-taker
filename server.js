bd6cb9d9ad3797ad9a9437fa10aaf29c06a99391
Camila Fernandez <ailenfernandez99@gmail.com>
2022-05-20 21:52:49 -0500

    Changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const nanoid = require('nanoid');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.static('public'));
 app.use(express.json());
